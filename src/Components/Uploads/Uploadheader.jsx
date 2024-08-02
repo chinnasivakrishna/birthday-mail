e9a3f79383facedf9bad3ffc378291e974048a64
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-08-02 12:55:02 +0530

    Update Uploadheader.jsx

--- src/Components/Uploads/Uploadheader.jsx
+++ src/Components/Uploads/Uploadheader.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import logo from '../photos/logo.png'
+import { useNavigate } from 'react-router-dom'
 
 const Uploadheader = () => {
+  const navigate = useNavigate();
+  const logout = () => {
+    navigate("/login", { replace: true });
+    
+    window.history.replaceState(null, '', '/login');
+  }
   return (
     <header style={{ "backgroundColor": "lightblue" }}>
       <nav>
         <div className="logo">
           <img src={logo} className='img' />
         
+        </div>
+        <div className="auth-buttons">
+          
+          <button className="download-button" onClick={logout} >Logout</button>
         </div>
       
         
